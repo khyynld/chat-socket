810dc9849359746473493bb42a3d5a81c16a8e08
mynamethis <khyynld@gmail.com>
2019-07-16 17:40:10 +0700

    format code

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ chat-socket nodejs express
 
 # how to run ?
 npm install
+
 npm start
 
 localhost:5000
--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var express = require('express'),
 server.listen(process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/chatui.html');
 });
 
-io.sockets.on('connection', function(socket){
-	socket.on('new user', function(name, data){
-			if (name in users){
-				data(false);
-			}else{
-				data(true);
+io.sockets.on('connection', function (socket) {
+	socket.on('new user', function (name, data) {
+		if (name in users) {
+			data(false);
+		} else {
+			data(true);
 			socket.nickname = name;
 			users[socket.nickname] = socket;
 			console.log('add nickName');
@@ -24,19 +24,29 @@ io.sockets.on('connection', function(socket){
 
 	});
 
-	function updateNickNames(){
+	function updateNickNames() {
 		io.sockets.emit('usernames', Object.keys(users));
 	}
-	socket.on('open-chatbox', function(data){
-		users[data].emit('openbox', {nick: socket.nickname});
+	socket.on('open-chatbox', function (data) {
+		users[data].emit('openbox', {
+			nick: socket.nickname
+		});
 	});
-	socket.on('send message',function(data, sendto){
-		users[sendto].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
-		users[socket.nickname].emit('new message',{msg: data, nick: socket.nickname, sendto: sendto});
+	socket.on('send message', function (data, sendto) {
+		users[sendto].emit('new message', {
+			msg: data,
+			nick: socket.nickname,
+			sendto: sendto
+		});
+		users[socket.nickname].emit('new message', {
+			msg: data,
+			nick: socket.nickname,
+			sendto: sendto
+		});
 
 		console.log(data);
 	});
-	socket.on('disconnect', function(data){
+	socket.on('disconnect', function (data) {
 		if (!socket.nickname) return;
 		delete users[socket.nickname];
 		updateNickNames();
